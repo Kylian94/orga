972db31ff119fb1d1c64a94c0c428311e57bd9e1
Kylian <kylian.petitgenet@hotmail.fr>
2020-07-03 13:54:08 +0200

    accept and refuse friend and display if not founded

--- src/screens/FriendsRequestScreen.js
+++ src/screens/FriendsRequestScreen.js
@@ -39,6 +39,44 @@ export default class FriendsRequest extends React.Component {
             });
     }
 
+    acceptFriend = (id) => {
+        return fetch('https://api-orga.kp-dev.fr/api/accept_friend/' + id, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + this.state.token,
+            },
+        }) // requête vers l'API
+            .then((response) => response.json())
+            .then((results) => {
+                //console.log("test")
+                console.log(results)
+
+            }).catch((error) => {
+                console.error(error);
+            });
+    }
+    cancelFriend = (id) => {
+        return fetch('https://api-orga.kp-dev.fr/api/delete_friend/' + id, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + this.state.token,
+            },
+        }) // requête vers l'API
+            .then((response) => response.json())
+            .then((results) => {
+                //console.log("test")
+                console.log(results)
+                //this.setState({ friends: results.user.friend_of })
+
+            }).catch((error) => {
+                console.error(error);
+            });
+    }
+
     render() {
         const { navigation } = this.props;
         if (this.state.friends == null) {
@@ -51,31 +89,46 @@ export default class FriendsRequest extends React.Component {
             return (
                 <View style={styles.container}>
                     <ScrollView style={styles.container}>
-                        {this.state.friends.map((friend, index) => {
-                            return (
-                                <View key={index} style={[{ display: "flex", flexDirection: "row" }, styles.marginTop20, styles.alignCenter, styles.justifyBetween, styles.marginBottom20]}>
-                                    <View style={[{ display: "flex", flexDirection: "row" }, styles.alignCenter]}>
-                                        <Image resizeMode={'cover'}
-                                            source={{ uri: 'https://i.stack.imgur.com/6FiRR.png' }}
-                                            style={styles.profil_picture} />
-                                        <Text style={[styles.marginLeft5]}>{friend.firstname}</Text>
-                                        <Text style={[styles.textBold, styles.marginLeft5]}>{friend.lastname}</Text>
-                                    </View>
-                                    <View style={[{ display: "flex", flexDirection: "row" }]}>
-                                        <TouchableOpacity style={[styles.btnGreen]}>
-                                            <Text style={[styles.textWhite, styles.textBold]}>
-                                                V
+                        {
+                            this.state.friends.length ?
+
+                                this.state.friends.map((friend, index) => {
+                                    return (
+                                        <View key={index} style={[{ display: "flex", flexDirection: "row" }, styles.marginTop20, styles.alignCenter, styles.justifyBetween, styles.marginBottom20]}>
+                                            <View style={[{ display: "flex", flexDirection: "row" }, styles.alignCenter]}>
+                                                <Image resizeMode={'cover'}
+                                                    source={{ uri: 'https://i.stack.imgur.com/6FiRR.png' }}
+                                                    style={styles.profil_picture} />
+                                                <Text style={[styles.marginLeft5]}>{friend.firstname}</Text>
+                                                <Text style={[styles.textBold, styles.marginLeft5]}>{friend.lastname}</Text>
+                                            </View>
+                                            <View style={[{ display: "flex", flexDirection: "row" }]}>
+                                                <TouchableOpacity style={[styles.btnGreen]} onPress={() => this.acceptFriend(friend.id)}>
+                                                    <Text style={[styles.textWhite, styles.textBold]}>
+                                                        V
                                 </Text>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity style={[styles.btnGreenOutLine, styles.marginLeft40]}>
-                                            <Text style={[styles.textGreen, styles.textBold]}>
-                                                X
+                                                </TouchableOpacity>
+                                                <TouchableOpacity style={[styles.btnGreenOutLine, styles.marginLeft40]} onPress={() => this.cancelFriend(friend.id)}>
+                                                    <Text style={[styles.textGreen, styles.textBold]}>
+                                                        X
                                 </Text>
-                                        </TouchableOpacity>
-                                    </View>
+                                                </TouchableOpacity>
+                                            </View>
+                                        </View>
+                                    )
+
+
+                                })
+                                :
+                                <View>
+                                    <Text style={[styles.textCenter, styles.marginTop20]}>Vous n'avez pas encore reçu de demande d'ami</Text>
+                                    <Text style={[styles.textCenter, styles.marginTop40, styles.textSecondary]}>Vous pouvez trouvez vos amis via l'outil de recherche</Text>
+                                    <TouchableOpacity onPress={() => navigation.navigate('SearchFriends')} style={[styles.btnGreen, styles.marginTop10, styles.marginBottom40, styles.textCenter]}>
+                                        <Text style={[styles.textBold, styles.textWhite, styles.textCenter, styles.marginBottom10, styles.marginTop10]}>Rechercher des amis</Text>
+                                    </TouchableOpacity>
                                 </View>
-                            )
-                        })}
+
+                        }
                     </ScrollView>
                 </View >
             )
--- src/screens/FriendsScreen.js
+++ src/screens/FriendsScreen.js
@@ -38,6 +38,25 @@ export default class Friends extends React.Component {
                 console.error(error);
             });
     }
+    cancelFriend = (id) => {
+        return fetch('https://api-orga.kp-dev.fr/api/delete_friend/' + id, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + this.state.token,
+            },
+        }) // requête vers l'API
+            .then((response) => response.json())
+            .then((results) => {
+                //console.log("test")
+                console.log(results)
+                //this.setState({ friends: results.user.friend_of })
+
+            }).catch((error) => {
+                console.error(error);
+            });
+    }
 
     render() {
         const { navigation } = this.props;
@@ -51,24 +70,35 @@ export default class Friends extends React.Component {
             return (
                 <View >
                     <ScrollView style={styles.container}>
-                        {this.state.friends.map((friend, index) => {
-                            return (
-                                <View key={index} style={[{ display: "flex", flexDirection: "row" }, styles.marginTop20, styles.alignCenter, styles.justifyBetween, styles.marginBottom20]}>
-                                    <View style={[{ display: "flex", flexDirection: "row" }, styles.alignCenter]}>
-                                        <Image resizeMode={'cover'}
-                                            source={{ uri: 'https://i.stack.imgur.com/6FiRR.png' }}
-                                            style={styles.profil_picture} />
-                                        <Text style={[styles.marginLeft5]}>{friend.firstname}</Text>
-                                        <Text style={[styles.textBold, styles.marginLeft5]}>{friend.lastname}</Text>
-                                    </View>
-                                    <TouchableOpacity style={[styles.btnGreenOutLine]}>
-                                        <Text style={[styles.textGreen, styles.textBold]}>
-                                            X
+                        {
+                            this.state.friends.length ?
+                                this.state.friends.map((friend, index) => {
+                                    return (
+                                        <View key={index} style={[{ display: "flex", flexDirection: "row" }, styles.marginTop20, styles.alignCenter, styles.justifyBetween, styles.marginBottom20]}>
+                                            <View style={[{ display: "flex", flexDirection: "row" }, styles.alignCenter]}>
+                                                <Image resizeMode={'cover'}
+                                                    source={{ uri: 'https://i.stack.imgur.com/6FiRR.png' }}
+                                                    style={styles.profil_picture} />
+                                                <Text style={[styles.marginLeft5]}>{friend.firstname}</Text>
+                                                <Text style={[styles.textBold, styles.marginLeft5]}>{friend.lastname}</Text>
+                                            </View>
+                                            <TouchableOpacity style={[styles.btnGreenOutLine]} onPress={() => this.cancelFriend(friend.id)}>
+                                                <Text style={[styles.textGreen, styles.textBold]}>
+                                                    X
                                         </Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    )
+                                })
+                                :
+                                <View>
+                                    <Text style={[styles.textCenter, styles.marginTop20]}>Vous n'avez pas encore reçu de demande d'ami</Text>
+                                    <Text style={[styles.textCenter, styles.marginTop40, styles.textSecondary]}>Vous pouvez trouvez vos amis via l'outil de recherche</Text>
+                                    <TouchableOpacity onPress={() => navigation.navigate('SearchFriends')} style={[styles.btnGreen, styles.marginTop10, styles.marginBottom40, styles.textCenter]}>
+                                        <Text style={[styles.textBold, styles.textWhite, styles.textCenter, styles.marginBottom10, styles.marginTop10]}>Rechercher des amis</Text>
                                     </TouchableOpacity>
                                 </View>
-                            )
-                        })}
+                        }
                     </ScrollView>
                 </View >
             )
